a8a436f729242e6f90b186ff1849d11dea173667
Vasavi <vasavi.gitla@yahoo.com>
2021-01-12 14:09:30 -0600

    finished

---
+++ src/exercises/DataTypes_Alice.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class DataTypes_Alice {
+    public static void main (String[] args){
+        DataTypes_Alice myFirstDataTypesAlice = new DataTypes_Alice();
+        myFirstDataTypesAlice.myFirstMethod();
+    }
+//    private String myFirstClassVariable = "This is a string";
+
+    public void myFirstMethod(){
+        System.out.println("Hello world");
+    }
+}
